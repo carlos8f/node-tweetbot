42f630938ba0f4ebe73f0df01b8c4683d05b87a7
Carlos Rodriguez <carlos@s8f.org>
2013-07-04 17:38:57 -0700

    update

--- index.js
+++ index.js
@@ -1,65 +1,81 @@
-var twitter = require('ntwitter')
+var modeler = require('modeler-twitter')
+  , ntwitter = require('ntwitter')
   , markov = require('markov')
-  , Stream = require('stream')
-  ;
+  , natural = require('natural')
 
-module.exports = function(options) {
-  var tw = new twitter(options)
-    , m = markov(1)
-    , tweetBase = {}
-    , loadTimeout
-    ;
+module.exports = function (options) {
+  var home = modeler({
+    name: 'tweets',
+    oauth: options.oauth
+  });
 
-  function onTweet(tweet) {
-    if (tweet && tweet.text) {
-      m.seed(tweet.text);
-    }
+  var twitter = new ntwitter({
+    consumer_key: options.oauth.consumer_key,
+    consumer_secret: options.oauth.consumer_secret,
+    access_token_key: options.oauth.token,
+    access_token_secret: options.oauth.token_secret
+  });
+
+  // @todo: classify hashtags
+  var classifier = new natural.BayesClassifier();
+  function classifyHashtags (text) {
+
   }
 
-  function removeMentions(words) {
-    if (options.removeMentions) {
-      words.forEach(function(word, k) {
-        if (word.indexOf('@') === 0) {
-          words[k] = word.substring(1);
+  function stripMentions (words) {
+    return words.filter(function (word) {
+      return !!word.match(/^@/);
+    });
+  }
+
+  function createTweet (words, maxLength) {
+    maxLength || (maxLength = 140);
+    var text = '';
+    for (var idx = 0; idx < words.length; idx++) {
+      var tmp = text + ' ' + words[idx];
+      if (tmp.length > 140) break;
+      text = tmp;
+    }
+    return text;
+  }
+
+  twitter.verifyCredentials(function (err, user) {
+    if (err) throw err;
+
+    var m = markov();
+
+    home.tail(0, {load: true}, processChunk);
+
+    function processChunk (err, chunk, next) {
+      if (err) throw err;
+      chunk.forEach(function (tweet) {
+        m.seed(tweet.text);
+      });
+      if (chunk.length && next) next();
+    }
+
+    twitter.stream('user', {'with': 'followings', 'replies': 'all'}, function (stream) {
+      stream.on('data', function (data) {
+        if (data.text) {
+          m.seed(data.text, function () {
+            if (data.in_reply_to_screen_name === user.screen_name) {
+              // respond
+              var words = ['@' + data.user.screen_name].concat(stripMentions(m.respond(data.text, 100)));
+              var text = createTweet(words);
+              twitter.updateStatus(text, function (err, data) {
+                if (err) throw err;
+                console.log('tweeted:', data.text || data);
+              });
+            }
+          });
         }
       });
-    }
-    return words;
-  }
-
-  tw
-    .getHomeTimeline(function(err, data) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      Object.keys(data).forEach(function(k) {
-        onTweet(data[k]);
+      stream.on('end', function (response) {
+        // Handle a disconnection
       });
-      tweetSomething();
-    })
-    .stream('user', function(stream) {
-      stream.on('data', onTweet);
-      stream.on('error', function(err) {
-        console.error(err);
-        process.exit(1);
-      });
-      stream.on('end', function() {
-        console.error('Socket connection to Twitter ended! Exiting.');
-        process.exit();
+      stream.on('destroy', function (response) {
+        // Handle a 'silent' disconnection from Twitter, no end/error event fired
       });
     });
-
-  function tweetSomething() {
-    var tweet = removeMentions(m.fill(m.pick(), Math.round((Math.random() * 100) + 20))).join(' ');
-    tw.updateStatus(tweet, function(err, data) {
-      if (err) {
-        console.error('tried to tweet: ' + tweet);
-        console.error(err);
-        process.exit(1);
-      }
-      console.log('tweeted: ' + tweet);
-    });
-    setTimeout(tweetSomething, options.tweetInterval || 1000 * 60 * 60);
-  }
+  });
 };
